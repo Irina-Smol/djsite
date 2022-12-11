fe22ddf4a882484610999bcd2bacd903646c6569
Ирина <Smolenskaya2002@yandex.ru>
2022-12-12 01:39:09 +0300

    Create urls.py

---
+++ coolsite/coolsite/urls.py
@@ -0,0 +1,46 @@
+"""coolsite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls.static import static
+
+from coolsite import settings
+from django.contrib import admin
+
+from django.urls import path, include
+from women.views import *
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post'),
+    path('category/<int:cat_id>/', show_category, name='category'),
+]
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#    path('', include('women.urls')),
+#    path('addpage/', addpage, name='add_page'),
+#    path('contact/', contact, name='contact'),
+#    path('login/', login, name='login'),
+#    path('post/<int:post_id>/', show_post, name='post'), # формирование динамического URL
+#    path('category/<int:cat_id>/', show_category, name='category'),
+#]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA__ROOT)
+
+handler404 = pageNotFound #подключение обработки исключений
